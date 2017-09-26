f00a5565e5884f1cf25ca24140358c89f747ea3c
sumakumardey <bappi.5476@gmail.com>
2017-09-26 18:23:31 +0800

    adding asg example

---
+++ chapter_2/asg/main.tf
@@ -0,0 +1,52 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+data "aws_availability_zones" "all" {}
+
+variable "server_port" {
+description = "This is used for listening"
+default = 8080
+}
+
+resource "aws_security_group" "instance"{
+ name = "terraform-example-instance"
+ ingress {
+  from_port = "${var.server_port}"
+  to_port = "${var.server_port}"
+  protocol = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+ }
+ 
+ lifecycle{
+   create_before_destroy = true
+ }
+}
+
+resource "aws_launch_configuration" "example" {
+  image_id = "ami-40d28157"
+  instance_type = "t2.micro"
+  security_groups = ["${aws_security_group.instance.id}"]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p "${var.server_port}" &
+              EOF
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = "${aws_launch_configuration.example.id}"
+  availability_zones = ["${data.aws_availability_zones.all.names}"]
+  min_size = 2
+  max_size = 10
+  
+ tag {
+ key = "Name"
+ value = "terraform-asg-example"
+ propagate_at_launch = true
+ }
+}
