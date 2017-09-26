9d8323edf6f35cb415763363ff1f3596c22d9f91
sumakumardey <bappi.5476@gmail.com>
2017-09-26 17:44:07 +0800

    adding security group

--- chapter_2/single_server/main.tf
+++ chapter_2/single_server/main.tf
@@ -1,12 +1,33 @@
 provider "aws" {
 	region = "us-east-1"
 }
-
+variable "server_port" {
+  description = "The port the server will use for http servers"
+  default = 8080
+}
+resource "aws_security_group" "instance" {
+ name = "terraform-example-instance"
+  ingress {
+    from_port = "${var.server_port}"
+    to_port = "${var.server_port}"
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 resource "aws_instance" "example"{
 	ami = "ami-40d28157"
 	instance_type = "t2.micro"
-
+        vpc_security_group_ids = ["${aws_security_group.instance.id}"]
 	tags {
 	Name = "terraform-example"
 	}
+        user_data = <<-EOF
+                    #!bin/bash
+                    echo "Hello, World"> index.html
+                    nohup busybox httpd -f -p "${var.server_port}" &
+                  EOF
+}
+
+output "public_ip_instance" {
+  value = "${aws_instance.example.public_ip}"
 }
