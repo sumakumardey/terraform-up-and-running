841bdddb71d5c2900723e42a00337457edcfdfe8
sumakumardey <bappi.5476@gmail.com>
2017-09-27 13:47:56 +0800

    chapter3

---
+++ chapter3/remote_config/main.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+ region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "terraform-up-and-running-suman"
+
+  versioning {
+    enabled = true
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+
+}
---
+++ chapter3/terraform_state/terraform_temp.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+data "terraform_remote_state" "network" {
+   backend = "s3"
+   config {
+     bucket = "terraform-up-and-running-suman"
+     key = "network/terraform.tfstate"
+     region  = "us-east-1"
+   }
+}
+
+resource "aws_instance" "example" {
+  ami = "ami-40d28157"
+  instance_type = "t2.micro"
+}
